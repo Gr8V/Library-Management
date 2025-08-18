f7e0d76381e1004aeac9e576b78cd198ed192b8a
Gr8V <vanshdangi0045@gmail.com>
2025-08-18 22:07:32 +0530

    added userlogin func

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,32 @@
 
 using namespace std;
 
+bool isAdmin = false;
+
+void UserLogin();
+
 int main()
 {
-    cout << "Hello World!!!";
+    UserLogin();
 
     return 0;
+}
+
+void UserLogin()
+{
+    string username;
+    string inputPasswd;
+    const string adminPasswd = "qwerty";
+
+    cout << "username: ";
+    getline(cin, username);
+    if (username == "admin")
+    {
+        cout << "password: ";
+        cin >> inputPasswd;
+        if (inputPasswd == adminPasswd)
+        {
+            isAdmin = true;
+        }
+    }
 }
